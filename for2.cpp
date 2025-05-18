e299012b9d7ecd5a753a2c490b3c19591aec2077
Keiryn21 <00061225@uca.edu.sv>
2025-05-18 12:34:24 -0600

    Add files via upload

---
+++ for2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+
+    //Debemos mostrar los numero pares de 1 al 50//
+
+    cout<<"a continuacion veras los numero oares del 1 al 50"<<endl;
+    for (int i=2;i<50;i+=2){
+        cout<<i<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
